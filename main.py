f85ee30a2efefe52c05fef3ff4c6b8d3f6303894
kuanyi-ng <raionkurane@gmail.com>
2023-04-09 17:02:07 +0900

    refactor compare_edge() as extract_frames_with_diff_edge()

--- main.py
+++ main.py
@@ -21,58 +21,40 @@ def save_frame_as_image(output_dir: str, frame: npt.NDArray, n: int) -> None:
     else:
         logger.info(f"Saved file: {file_path}")
 
-def compare_edge(
-        cap: cv2.VideoCapture,
-        prev_frame: cv2.VideoCapture,
-        threshold: float = 1,
-        output_dir: str = 'output',
-    ):
-    frame_ids = [0]
-    frames = []
+def extract_frames_with_diff_edge(frames: list, threshold: float = 1):
+    frame_with_diff_edges_ids = [0]
 
-    # Loop through each frame of the video
-    while True:
-        # Read the next frame
-        ret, frame = cap.read()
-        frames.append(frame)
-        
-        # Break the loop if we've reached the end of the video
-        if not ret:
-            break
-        
-        # Convert the frame to grayscale and calculate the absolute difference
+    first_frame = frames[0]
+    prev_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+    prev_edges = cv2.Canny(prev_frame, 100, 200)
+
+    # Find frames where changes in edges happen
+    for frame_id, frame in enumerate(frames, 1):
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
         edges = cv2.Canny(gray_frame, 100, 200)
-        prev_edges = cv2.Canny(prev_frame, 100, 200)
-        abs_diff = cv2.absdiff(edges, prev_edges)
-        
+
         # Calculate the mean of the absolute difference
+        abs_diff = cv2.absdiff(edges, prev_edges)
         mean_diff = abs_diff.mean()
-        print(f"{cap.get(cv2.CAP_PROP_POS_FRAMES)},{mean_diff}")
 
-        # If the mean difference is greater than the threshold, output the frame
         if mean_diff > threshold:
-            frame_ids.append(cap.get(cv2.CAP_PROP_POS_FRAMES))
-            # save_frame_as_image(output_dir, cap, frame)
+            frame_with_diff_edges_ids.append(frame_id)
 
-        # Update the previous frame
-        prev_frame = gray_frame
-    frames_to_extract = []
-    for i, left_frame_id in enumerate(frame_ids):
-        if i + 1 == len(frame_ids):
-            break
+        prev_edges = edges
 
-        right_frame_id = frame_ids[i+1]
-        frames_to_extract.append((left_frame_id + right_frame_id) // 2)
-    
-    return_frames = []
-    for i in frames_to_extract:
-        return_frames.append(frames[int(i)])
-    return return_frames
+    # Representative frames are between two consecutive frames with different edges
+    representative_frame_ids = []
+    for i, left_frame_id in enumerate(frame_with_diff_edges_ids[:-1]):
+        right_frame_id = frame_with_diff_edges_ids[i+1]
+
+        representative_frame_ids.append((left_frame_id + right_frame_id) // 2)
+
+    logging.debug(f"e: ({len(representative_frame_ids)} {representative_frame_ids}")
+
+    # TODO: want to include `first_frame` as the first element
+    return [ frames[int(frame_i)] for frame_i in representative_frame_ids ]
 
 def remove_similar_frames(
-        initial_frame,
         frames: list,
         threshold: int = 30
 ):
@@ -80,8 +62,9 @@ def remove_similar_frames(
     transition_frame_ids = []
 
     # detect where changes happen
+    initial_frame = frames[0]
     prev_grey_transition_frame = cv2.cvtColor(initial_frame, cv2.COLOR_BGR2GRAY)
-    for frame_id, frame in enumerate(frames):
+    for frame_id, frame in enumerate(frames, 1):
         # Convert the frame to grayscale and calculate the absolute difference
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -93,23 +76,22 @@ def remove_similar_frames(
             transition_frame_ids.append(frame_id)
             prev_grey_transition_frame = gray_frame
 
-    print(transition_frame_ids)
+    print(f"t: {transition_frame_ids}")
 
     # find the middle frame between each transition
     distinct_frames = []
     for i, left_frame_id in enumerate(transition_frame_ids[:-1]):
         right_frame_id = transition_frame_ids[i+1]
-        frame_id = (left_frame_id + right_frame_id) // 2
+
+        frame_id = (left_frame_id + right_frame_id) // 2 - 1
 
         distinct_frames.append(frames[frame_id])
 
     return distinct_frames
 
 def main():
-    threshold = 30
     # Load the video file
     cap = cv2.VideoCapture('sample.mp4')
-    logger.info(cap)
 
     # Create the output directory if it doesn't exist
     output_dir = 'output'
@@ -117,18 +99,22 @@ def main():
         os.makedirs(output_dir)
 
     # Read the first frame and output it unconditionally
-    _, frame = cap.read()
-    save_frame_as_image(output_dir, frame, 0)
+    # _, first_frame = cap.read()
+    # print(cap.get(cv2.CAP_PROP_POS_FRAMES))
+    # save_frame_as_image(output_dir, first_frame, 0)
 
-    # Define the initial frame to compare against
-    prev_output_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # Loop through each frame of the video
-    # edge 検知されて、抽出したframe
-    edges = compare_edge(cap, prev_output_frame, output_dir=output_dir)
+        frames.append(frame)
 
-    ff = remove_similar_frames(frame, edges, threshold=30)
-    print(ff)
+    edges = extract_frames_with_diff_edge(frames)
+
+    # edges.insert(0, first_frame)
+    ff = remove_similar_frames(edges, threshold=30)
     for i, frame in enumerate(ff):
         save_frame_as_image(output_dir, frame, (i + 1) * 200)
 
